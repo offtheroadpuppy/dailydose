1958f97c2e70b8310a1a32d487f547a0b05cd0b3
offtheroadpuppy <pimenow.al@gmail.com>
2026-08-11 20:53:16 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'DailyDose-cache';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// 1. Install Event: Cache essential assets locally
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+
+// 2. Fetch Event: Intercept network calls to serve cached files when offline
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
